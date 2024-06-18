e3834a291145f442df14df3f4a6e36cf94aabbf2
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-18 21:33:33 +0300

    update

--- project-backend/src/index.js
+++ project-backend/src/index.js
@@ -1,5 +1,11 @@
 // console.log('JHello');
 // import { releaseYearRegexp } from './constants/movies-constants.js';
 // console.log(releaseYearRegexp.test('2000'));
+import initMongoDB from './db/nitMongoDB.js';
 import startServer from './server.js';
-startServer();
+
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+bootstrap();
