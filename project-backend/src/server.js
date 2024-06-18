6648f6396365995d22345d469c17714e189be098
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-18 23:01:51 +0300

    update

--- project-backend/src/server.js
+++ project-backend/src/server.js
@@ -60,7 +60,7 @@ import express from 'express';
 import cors from 'cors';
 import movies from './db/movies.js';
 import pino from 'pino-http';
-
+import { getMovies, getMovieById } from './services/movie-services.js';
 const startServer = () => {
   const app = express();
   const logger = pino({
@@ -70,9 +70,40 @@ const startServer = () => {
   });
   app.use(cors());
   app.use(logger);
-  app.get('/api/movies', (req, res) => {
-    res.json(movies);
-  });
+
+  app.get('/api/movies', async (req, res) => {
+    const data = await getMovies();
+    res.json({
+      status: 200,
+      data,
+      message: 'Success',
+    });
+  }),
+    app.get('/api/movies/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+        const data = await getMovieById(id);
+        if (!data) {
+          return res.status(404).json({
+            message: 'Movie  not found',
+          });
+        }
+
+        res.json({
+          status: 200,
+          data,
+          message: `Success with ${id}`,
+        });
+      } catch (error) {
+        if (error.message.includec('Cast to ObjectId failed')) {
+          error.status = 404;
+        }
+        const { status = 500 } = error;
+        res.status(status).json({
+          message: error.message,
+        });
+      }
+    });
   app.use((req, res) => {
     res.status(404).json({
       message: 'Not found',
